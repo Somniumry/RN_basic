ed68165d38c72d8b65b806e73cbcc30f11395ddf
ranyoung.kim <58300117+fks1311@users.noreply.github.com>
2022-01-10 21:42:50 +0900

    init

---
+++ navigation/Tabs.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Movie from '../screens/Movie';
+import Search from '../screens/Search';
+import TV from '../screens/TV';
+
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="movies" component={Movie} />
+            <Tab.Screen name="tv" component={TV} />
+            <Tab.Screen name="search" component={Search} />
+        </Tab.Navigator>
+    )
+}
+
+export default Tabs;
